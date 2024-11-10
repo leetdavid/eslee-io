bc264ed38ba2e3b77c952deff8e9cebe100c0dfc
David Lee <leetdavidu@gmail.com>
2024-11-10 15:25:11 +0800

    fix database

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,10 +1,16 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 import * as schema from "./schema";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("Missing DATABASE_URL");
+}
+
+const client = postgres(process.env.DATABASE_URL);
+
 export const db = drizzle({
-  client: sql,
+  client,
   schema,
   casing: "snake_case",
 });
