7158ecbfa94b03ee52fc2679cdefc4a8f8798c04
David Lee <leetdavidu@gmail.com>
2026-01-18 16:17:08 +0800

    Remove root mapping

--- infra/main.tf
+++ infra/main.tf
@@ -1,12 +1,3 @@
-# ? https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/dns_record
-resource "cloudflare_dns_record" "eslee_io" {
-  zone_id = var.cloudflare_zone_id
-  name    = "@"
-  content = "76.76.21.21"
-  type    = "A"
-  ttl     = 1
-}
-
 locals {
   vercel_cnames_map = {
     cms = {
@@ -17,6 +8,7 @@ locals {
 }
 
 # ? Dynamic CNAME records that point to the new vercel DNS records
+# ? https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs/resources/dns_record
 resource "cloudflare_dns_record" "eslee_io_vercel_cnames_map" {
   for_each = local.vercel_cnames_map
 
